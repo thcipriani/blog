caf683eb800a4ba121f95288e7fed5d67bd9735f
Tyler Cipriani <thcipriani@gmail.com>
2011-02-16 09:25:26 -0800

    Devise for commenters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-before_filter :authenticate, :only => :destroy
+before_filter :authenticate_admin!
 
 	def create
 		@post = Post.find(params[:post_id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  devise_for :admins
+
   resources :posts do
 	resources :comments
   end
